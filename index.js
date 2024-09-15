af8f60b63966a6b9b7d9b31b482dc803aa2e6491
Brianna Dunfield <bdunfield@mta.ca>
2024-09-15 11:31:44 -0300

    Initial server set up, added controller files

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+// import medicationRouter from './routes/medications.js';
+
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+
+// app.use('/medications', medicationRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Server is listening on 8080');
+});
